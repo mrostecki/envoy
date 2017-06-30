041a83bf842de081d4c813af8ce0c10f2925b6c6
Jarno Rajahalme <jarno@covalent.io>
2017-06-30 14:44:18 -0700

    listener: Use actual local address.
    Use the local address of the new socket instead of the address of the
    listener, as the latter may be an all-hosts address.  The actual
    address is needed to deduce if the address returned from
    SO_ORIGINAL_DST is the same as the destination or not.
    
    The comparison needs to be done on the actual addresses rather than
    pointers to them, as the address returned from getOriginalDst() is
    always a new Instance.  In this case the pointers always compare as
    different, even though the actual addresses may be the same.
    
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

--- source/common/network/proxy_protocol.cc
+++ source/common/network/proxy_protocol.cc
@@ -111,7 +111,9 @@ void ProxyProtocol::ActiveConnection::onReadWorker() {
   removeFromList(parent_.connections_);
 
   // TODO(mattklein123): Parse the remote port instead of passing zero.
-  listener.newConnection(fd, remote_address, listener.socket().localAddress());
+  // TODO(jrajahalme): Use destination address from proxy protocol instead of the local
+  //                   address of the socket?
+  listener.newConnection(fd, remote_address, Address::addressFromFd(fd_));
 }
 
 void ProxyProtocol::ActiveConnection::close() {
